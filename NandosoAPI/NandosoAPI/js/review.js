d79a5a555b2c889d32dc6d8a3cf30acae44e03aa
Louis Thie <louisthie@orcon.net.nz>
2015-11-23 20:46:19 +1300

    new review page almost done, review.js not started but created, rest of the website essentially finished

---
+++ NandosoAPI/NandosoAPI/js/review.js
@@ -0,0 +1,5 @@
+﻿function submit() {
+    var name = document.getElementById("nameBox");
+    var score = document.getElementById("scoreBox");
+    var review = document.getElementById("reviewBox");
+}
